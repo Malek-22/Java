d6dad1c049b3306d186b36306ac3630534db5318
Malek-22 <malekrom111@gamil.com>
2025-05-15 18:30:50 +0100

    Java

---
+++ First Java Program/MalekInfo.java
@@ -0,0 +1,7 @@
+public class MalekInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Malek Romdhani");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Bizerte, Tunisia");
+    }
+}
