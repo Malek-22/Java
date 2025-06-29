cd8fddd90d731796ced0967195ec6487ac689b9e
Malek-22 <malekrom111@gamil.com>
2025-06-29 17:02:54 +0100

    Login & Registration

---
+++ Java Spring/loginandregistration/src/main/java/com/loginandregistration/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.loginandregistration.loginandregistration.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.loginandregistration.loginandregistration.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
